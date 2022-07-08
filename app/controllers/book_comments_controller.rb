067e6d7892dddbc60adb1aba533e99b0e4f1a750
EC2 Default User <ec2-user@ip-172-31-40-165.ec2.internal>
2022-07-08 08:25:27 +0000

    finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
+    @books = Book.all
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book.id)
+    # app/views/book_comments/create.js.erbを参照する
   end
 
   def destroy
+  @books = Book.all
   BookComment.find(params[:id]).destroy
-  redirect_to book_path(book.id)
+  # app/views/book_comments/destroy.js.erbを参照する
   end
 
     private
